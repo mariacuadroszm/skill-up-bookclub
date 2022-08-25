e4a5ac981b1ffdd31bb66ac979ea7dea123510fa
Maria del Mar Cuadros <mariadelmar.cuadros@zemoga.com>
2022-08-25 12:25:33 -0500

    feat-ZMB-123 - Add router guard when the user is not logged

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import SignUpView from "../views/SignUpView.vue";
 import LogInView from "../views/LogInView.vue";
 import ProposedBookListExtended from "../views/ProposedBookListExtended.vue";
 import ActiveClubsListExtended from "../views/ActiveClubsListExtended.vue";
+import EventService from "../services/EventService";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,6 +38,17 @@ const router = createRouter({
       props: true,
     },
   ],
+});
+
+router.beforeEach(async (to, from) => {
+  try {
+    const isUserLogged = await EventService.checkUserSesion();
+    if (!isUserLogged && to.name !== "log-in" && to.name !== "sign-up") {
+      return { name: "log-in" };
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 export default router;
